c68323e32686780edf74bf919b66642cb5f2fae9
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-09-22 10:35:34 +0200

    Minor changes

--- OpenID_poc1/OpenID_poc1/Controllers/AuthController.cs
+++ OpenID_poc1/OpenID_poc1/Controllers/AuthController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using OpenID_poc1.Services;
---
+++ OpenID_poc1/OpenID_poc1/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OpenID_poc1.Services
+{
+    public interface IAuthService
+    {
+        string GetUserEmail(ClaimsPrincipal user);
+        string GetUserName(ClaimsPrincipal user);
+
+    }
+}
